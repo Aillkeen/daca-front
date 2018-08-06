83a9c30a4cbe6bb7c09ed25118a0d9e51eee9747
Aillkeen <aillkeenoliveira@gmail.com>
2018-08-06 19:35:52 -0300

    Alteracao para deploy no heroku

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ path = require('path');
 const app =express();
 
 
-app.use(express.static('./dist/tira-duvida'));
+app.use(express.static('./tira-duvida'));
 
 app.get('/*', (req,res)=>{
 
-res.sendFile(path.join(__dirname,'/dist/tira-duvida/index.html'));
+res.sendFile(path.join(__dirname,'/tira-duvida/index.html'));
 
 });
 
